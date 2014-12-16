c4202a24e6e8a06d9633d516642bf7c18de1f0f5
Matthew Dahlhausen <mdahl14@umd.edu>
2014-12-16 14:43:49 -0500

    updated run_generation to handle hardsizing

--- analysis/run_generation.rb
+++ analysis/run_generation.rb
@@ -21,67 +21,90 @@ f5 = File.read("#{Dir.pwd}/base_scripts/5convert_to_idf.rb")
 f6 = File.read("#{Dir.pwd}/base_scripts/6add_energyplus_measures.rb")
 f7 = File.read("#{Dir.pwd}/base_scripts/7set_tools.rb")
 f9 = File.read("#{Dir.pwd}/base_scripts/9start_run.rb")
-
-# script reference to special hardsizing measure
+f_boiler = File.read("#{Dir.pwd}/base_scripts/boiler_hardsize_measure.rb")
+f_cu = File.read("#{Dir.pwd}/base_scripts/cu_hardsize_measure.rb")
 
 fname_runs = "#{Dir.pwd}/measures/unique_sims.txt"
 fname_map = "#{Dir.pwd}/measures/measure_map.txt"
 
-# file lookup for measures
-measure_table = []
+# has table for measure file lookup
+measure_hash = Hash.new
+dependence_hash = Hash.new
 File.open(fname_map) do |f|
   f.each_line do |line|
-    measure_table << line.split(' ')
+    a = line.split(' ')
+	key = "#{a[0]}"
+	measure_hash[key] = a[1]
+	dependence_hash[key] = a[2]
   end
 end
-# puts "\nARRAY:\n",measure_table.inspect,"\n"
-letters = measure_table.map { |r| r[0] }
-measures = measure_table.map { |r| r[1] }
-dependence = measure_table.map { |r| r[2] }
 
 # for each unique simulation, construct a run_script
 File.readlines(fname_runs).each do |seq|
-
-  # create array of measures in order
-  runs = seq.split('_')
-
-  runs.each do |run|
-    index = letters.index("#{run.strip}")
-    run.replace "#{measures[index]}"
-  end
-
+  
   # set output file directory for the script
   script_fname = "#{Dir.pwd}/run_scripts/#{seq.strip}.rb"
-
+  
   # output directory for the run results
-  f8 = 'outdir = OpenStudio::Path.new("#{Dir.pwd}/run_scripts/results/' +
-          "#{seq.strip}" '")'
+  f8 = 'outdir = OpenStudio::Path.new("#{Dir.pwd}/run_scripts/results/' + "#{seq.strip}" '")'
 
+  # open and file and write the run script to it   
   File.open(script_fname, 'w') do |mergedFile|
-    mergedFile << f1 + "\n" + f2 + "\n" + f3 + "\n\n" + '# START OSM MEASURES' + "\n"
+    mergedFile << f1 + "\n" + f2 + "\n" + f3 + "\n\n" + '# ***START OSM MEASURES***' + "\n"
+	
+	# create array of measures in order
+    runs = seq.split('_')
+	runs = runs.collect { |x| x.strip }
 
-      runs.each do |run|
-        # If a measure is dependent on preceding simulations,
-        # hardsize the equipment based on the .eio file of the prior simulation.
-        default_fname = "#{Dir.pwd}/measures/" + "#{run}" + "/default_script.rb"
-        mergedFile << File.read(default_fname)
+	# add each measure to model in order
+	index = 0
+    runs.each do |run|
+	
+	  if index == 0
+	    preceding_sim = "baseline"
+		preceding_measure = "baseline"
+	  else
+	    preceding_sim = "#{runs.slice(0,index).join("_")}"
+		preceding_measure = runs[index-1]
+	  end
+      #puts "INDEX: #{index}, runs: #{preceding_sim}"
+	  #puts "preceding_measure: #{preceding_measure}"
+	  
+	  preceding_files = Dir.entries("#{Dir.pwd}/run_scripts/results/" + "#{preceding_sim}")
+	  preceding_eplus_folder = preceding_files.keep_if {|x| x.include? "EnergyPlus"}
+	  preceding_eplus_folder = "#{Dir.pwd}/run_scripts/results/" + "#{preceding_sim}" + "/" + "#{preceding_eplus_folder[0]}" +"/"
+	  
+	  # if an HVAC measure is preceding, hardsize model based on prior run	  
+      if dependence_hash[preceding_measure].to_i == 1
+		if measure_hash[preceding_measure].include? "Boiler"
+	      sql_path = "#{preceding_eplus_folder}" + "eplusout.sql"
+		  mergedFile << "sql_path =" + "'#{sql_path}'"
+		  mergedFile << f_boiler + "\n\n"
+		end
+		if measure_hash[preceding_measure].include? "TwoSpeedDXCooling"
+	      eio_path = "#{preceding_eplus_folder}" + "eplusout.eio"
+		  mergedFile << "eio_path = #{eio_path}"
+		  mergedFile << f_cu + "\n\n"
+		end
       end
-
-    mergedFile << "\n" + '# END OSM MEASURES' + "\n\n" + f5 + "\n\n" +
-                              '# add EnergyPlus measures' + "\n" + f6 + "\n\n" +
-                              f7 + "\n" + '# ouput directory' + "\n\n" + f8 + "\n\n" + f9 + "\n"
+		
+      measure_file = "#{Dir.pwd}/measures/" + "#{measure_hash[run]}" + "/default_script.rb"
+	  mergedFile << File.read(measure_file) + "\n\n"
+	  index = index + 1
+    end
+	
+	# add remainder of reporting measures and output information
+    mergedFile << "\n" + '# ***END OSM MEASURES***' + "\n\n" + f5 + "\n\n" +
+                  '# add E+ measures' + "\n" + f6 + "\n\n" +
+                  f7 + "\n" + '# ouput directory' + "\n\n" + f8 + "\n\n" + f9 + "\n"
   end
-
 end
 
 # create the baseline run
-  # set output file directory for the script
-  script_fname = "#{Dir.pwd}/run_scripts/baseline.rb"
-  # output directory for the run results
-  f8 = 'outdir = OpenStudio::Path.new("#{Dir.pwd}/run_scripts/results/' +
-          "baseline" '")'
-  File.open(script_fname, 'w') do |mergedFile|
-    mergedFile << f1 + "\n" + f2 + "\n" + f3 + "\n" + f5 + "\n\n" +
-                  '# add EnergyPlus measures' + "\n" + f6 + "\n\n" +
-                  f7 + "\n" + '# ouput directory' + "\n\n" + f8 + "\n\n" + f9 + "\n"
-  end
+script_fname = "#{Dir.pwd}/run_scripts/baseline.rb"
+f8 = 'outdir = OpenStudio::Path.new("#{Dir.pwd}/run_scripts/results/' + "baseline" '")'
+File.open(script_fname, 'w') do |mergedFile|
+  mergedFile << f1 + "\n" + f2 + "\n" + f3 + "\n" + f5 + "\n\n" +
+                '# add EnergyPlus measures' + "\n" + f6 + "\n\n" +
+                f7 + "\n" + '# ouput directory' + "\n\n" + f8 + "\n\n" + f9 + "\n"
+end
